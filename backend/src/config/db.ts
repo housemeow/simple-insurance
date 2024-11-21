e7e9a760a74f76bcf6ecf98fd90e0124b1868137
housemeow <housemeow@gmail.com>
2024-11-21 21:39:39 +0800

    chore: mongo container persistent storage

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async (): Promise<void> => {
-    console.log('DB connected');
     try {
         await mongoose.connect(process.env.MONGO_URI as string, {});
         console.log('MongoDB connected successfully');
     } catch (err: any) {
-        console.error(`fffError: ${err.message}`);
+        console.error(`Error: ${err.message}`);
         process.exit(1); // Exit process with failure
     }
 };
