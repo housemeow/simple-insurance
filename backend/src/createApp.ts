9c95b7d2bb7286be59cd62badbbb145557a1f059
housemeow <housemeow@gmail.com>
2024-11-24 23:24:06 +0800

    feat: global error handling

--- backend/src/createApp.ts
+++ backend/src/createApp.ts
@@ -5,40 +5,39 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import rootRouter from '@/routes'; // Import the root router
+import { MongooseError } from 'mongoose';
 
 export function createApp() {
     const app = express();
-    
+
     app.use(logger('dev'));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(cookieParser());
     app.use(express.static(path.join(__dirname, 'public')));
     app.use(cors());
-    
+
     app.get('/', async (req: Request, res: Response) => {
         res.json({
             now: new Date().toISOString(),
         })
     })
-    
+
     app.use('/api', rootRouter);
-    
+
     // catch 404 and forward to error handler
     app.use((req: Request, res: Response, next: NextFunction) => {
         next(createError(404));
     });
-    
+
     // error handler
     app.use((err: HttpError, req: Request, res: Response, _next: NextFunction) => {
-        // set locals, only providing error in development
-        res.locals.message = err.message;
-        res.locals.error = req.app.get('env') === 'development' ? err : {};
-        
-        // render the error page
-        res.status(err.status || 500);
-        res.render('error');
+      if (err instanceof MongooseError) {
+        return res.status(400).json({ message: err.message });
+      }
+
+      res.status(500).json({ message: err?.message || err });
     });
-    
+
     return app;
 }
--- backend/src/routes/policyholdersRouter.ts
+++ backend/src/routes/policyholdersRouter.ts
@@ -1,62 +1,70 @@
-import { Router, Request, Response } from 'express';
+import { Counter } from '@/models/counter';
+import { IPolicyholder, Policyholder } from '@/models/policyholder';
+import { Request, Response, NextFunction, Router } from 'express';
+import createError from 'http-errors';
 
 const router = Router();
 
-router.get('/', async (req: Request, res: Response) => {
-    const code = req.query.code as string;
-    const data = {
-        code,
-        name: '保戶姓名',
+export const asyncErrorHandler = (fn: (req: Request, res: Response) => Promise<any>) => (req: Request, res: Response, next: NextFunction) => {
+  Promise.resolve(fn(req, res)).catch(next);
+};
+
+router.get('/', asyncErrorHandler(async (req, res) => {
+  const code = req.query.code as string;
+  const data = {
+    code,
+    name: '保戶姓名',
+    registration_date: new Date(),
+    introducer_code: '介紹人保戶編號',
+    l: [
+      {
+        code: '左樹保戶編號',
+        name: '左樹保戶姓名',
         registration_date: new Date(),
-        introducer_code: '介紹人保戶編號',
-        l: [
-            {
-                code: '左樹保戶編號',
-                name: '左樹保戶姓名',
-                registration_date: new Date(),
-                introducer_code: '左樹介紹人保戶編號',
-            },
-        ],
-        r: [
-            {
-                code: '右樹保戶編號',
-                name: '右樹保戶姓名',
-                registration_date: new Date(),
-                introducer_code: '右樹介紹人保戶編號',
-            },
-        ],
-    }
-
-    res.json(data);
-})
-
-router.get('/:code/top', async (req: Request, res: Response) => {
-    const code = req.params.code;
-
-    const data = {
-        code,
-        name: '保戶姓名',
+        introducer_code: '左樹介紹人保戶編號',
+      },
+    ],
+    r: [
+      {
+        code: '右樹保戶編號',
+        name: '右樹保戶姓名',
         registration_date: new Date(),
-        introducer_code: '介紹人保戶編號',
-        l: [
-            {
-                code: '左樹保戶編號',
-                name: '左樹保戶姓名',
-                registration_date: new Date(),
-                introducer_code: '左樹介紹人保戶編號',
-            },
-        ],
-        r: [
-            {
-                code: '右樹保戶編號',
-                name: '右樹保戶姓名',
-                registration_date: new Date(),
-                introducer_code: '右樹介紹人保戶編號',
-            },
-        ],
-    }
+        introducer_code: '右樹介紹人保戶編號',
+      },
+    ],
+  }
 
-    res.json(data);
-})
+  res.json(data);
+}))
+
+router.get('/:code/top', asyncErrorHandler(async (req, res) => {
+  const code = req.params.code;
+
+  const data = {
+    code,
+    name: '保戶姓名',
+    registration_date: new Date(),
+    introducer_code: '介紹人保戶編號',
+    l: [
+      {
+        code: '左樹保戶編號',
+        name: '左樹保戶姓名',
+        registration_date: new Date(),
+        introducer_code: '左樹介紹人保戶編號',
+      },
+    ],
+    r: [
+      {
+        code: '右樹保戶編號',
+        name: '右樹保戶姓名',
+        registration_date: new Date(),
+        introducer_code: '右樹介紹人保戶編號',
+      },
+    ],
+  }
+
+  res.json(data);
+}))
+
 
 export default router;
